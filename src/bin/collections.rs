d1ab59f68269cab86b9bbaaa317f3baee36f565e
Massimo <massimo@coderpunk.tech>
2026-07-17 09:05:55 +1000

    collections

---
+++ src/bin/collections.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+fn main() {
+  let mut _vector: Vec<i32> = Vec::new();
+  _vector.push(2);
+  for i in &_vector {
+    println!("{}", i);
+  }
+  let _vector = vec![1,2,3,4];
+  let second = _vector.get(0);
+  if let Some(second) = second {
+    println!("{}", second);
+  }
+
+  let mut map = HashMap::new();
+  map.insert("John", 20);
+  let found = map.get("John");
+  if let Some(age) = found {
+    println!("{}", age);
+  }
+}
